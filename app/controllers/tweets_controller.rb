48be8a78daf50cc69e524719e2709f4846c72228
shin <5446shin@gmail.com>
2019-01-19 07:34:57 +0000

    データベースを作成、HTMLの英語の文字列を英語に変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,7 +28,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to @tweet, notice: 'Tweetしました！' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.html { redirect_to @tweet, notice: 'Tweetを更新しました。' }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.html { redirect_to tweets_url, notice: 'Tweetを削除しました。' }
       format.json { head :no_content }
     end
   end
